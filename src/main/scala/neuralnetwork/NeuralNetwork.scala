e3f51e47c384bd684dc087852cff4ee01440e5f3
Tomasz Kowal <tomekowal@gmail.com>
2012-11-04 11:42:51 +0100

    add toString method for NeuralNetwork

--- src/main/scala/neuralnetwork/NeuralNetwork.scala
+++ src/main/scala/neuralnetwork/NeuralNetwork.scala
@@ -1,17 +1,31 @@
 package neuralnetwork
 
 object NeuralNetwork {
-    //List of weigts under given neuron
+    //List of weights under given neuron
     type NeuronWeight = Double
     type NeuronWeights = List[NeuronWeight]
     type Layer = List[NeuronWeights]
     type Weights = List[Layer]
 
+    trait WeightsPrinter {
+        def weightsToString (weights: Weights): String =
+            (for (layer <- weights) yield
+                layerToString(layer) + "\n").mkString
+
+        def layerToString(layer: Layer): String =
+            (for (neuronWeights <- layer) yield
+                neuronWeightsToString(neuronWeights) + "| ").mkString
+
+        def neuronWeightsToString(neuronWeights: NeuronWeights): String =
+            (for (weight <- neuronWeights) yield
+                weight.toString + " ").mkString
+    }
+
     class Neuron(val activationFunction: Double => Double) {
         def calculate(x: Double): Double = activationFunction(x)
     }
 
-    class NeuralNetwork(val weights: Weights, activationFunction: Double => Double) {
+    class NeuralNetwork(val weights: Weights, activationFunction: Double => Double) extends WeightsPrinter{
         val neuron = new Neuron(activationFunction)
         def scalarProduct(l1: List[Double], l2: List[Double]) = (for {(x, y) <- l1 zip l2} yield x * y).sum
         def calculate(input: List[Double]) = {
@@ -30,5 +44,7 @@ object NeuralNetwork {
                 case Nil => throw new IllegalArgumentException("Weight list cannot be empty")
             }
         }
+        override def toString =
+            weightsToString(weights)
     }
 }
